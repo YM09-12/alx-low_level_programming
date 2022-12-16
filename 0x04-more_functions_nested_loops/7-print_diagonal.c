db70315150262a0d003eccb73e7dd04d4cd730b5
YM09-12 <yangamdk@outlook.com>
2022-12-15 17:17:35 -0800

    diagonal printing

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * main - check the code
+ *
+ * Return: Always 0
+ */
+int main(void)
+{
+	print diagonal(0);
+	print diagonal(2);
+	print diagonal(10);
+	print diagonal(-4);
+	return (0);
+}
