31804dc17f6dceecd8a357dea13b4787ddc67352
YM09-12 <yangamdk@outlook.com>
2022-12-31 14:14:47 -0800

    Isdigit

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * _isdigit - write function that check for a digit (0 through 9).
+ * @c: charto check
+ * Return: 0 or 1
+ */
+int _isdigit(int c)
+{
+if (c >= '0' && c <= '9')
+return (0);
+else
+return (0);
+}
