18a9aa199d685900e29583c8c737cc1d429192e6
YM09-12 <yangamdk@outlook.com>
2022-12-31 14:08:47 -0800

    Isupper

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -1,15 +1,17 @@
 #include "main.h"
 
 /**
- * _isupper - check for uppercase letter
- * @c: char to be checked
- * Return: 1 if c is uppercase, 0 if otherwise
+ * _isupper - uppercase letters
+ * @c: character to check
+ *
+ * Return: 0 or 1
  */
-
 int _isupper(int c)
+
 {
 if (c >= 'A' && c <= 'Z')
 return (1);
 else
 return (0);
+
 }
