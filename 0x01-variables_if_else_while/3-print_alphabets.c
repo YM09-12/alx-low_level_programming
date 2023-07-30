1c2100f38b52818752a83fff2dd8f8a9206ed1b0
YM09-12 <yangamdk@outlook.com>
2023-07-30 08:25:46 -0700

    Alphabet

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+  * main - prints alphabet in lowercase, then uppercase
+  * Return: Always (Success)
+  */
+
+int main(void)
+{
+	char c;
+	for (c = 'a'; c <= 'z'; c++)
+	{
+		putchar (c);
+	}
+	for (c = 'A'; c <= 'Z'; c++)
+	{
+		putchar (c);
+	}
+	putchar ('\n');
+	return (0);
+}
